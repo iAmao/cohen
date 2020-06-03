cec55a7a88c0e637a2da0d749d29f64ed2987b70
Inumidun <ashamao90@gmnail.com>
2020-06-03 03:00:56 +0100

    Parse and validate source URL

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use structopt::StructOpt;
-
+use url::{Url, ParseError};
 
 #[derive(Debug, StructOpt)]
 #[structopt(name="Cohen", about="Compose your music like the great Leonard Cohen!")]
@@ -16,5 +16,27 @@ struct ConfigArgs {
 
 fn main() {
    let args = ConfigArgs::from_args();
+
+   if let Err(e) = validate_url(&args.source) {
+   		eprintln!("Source is not a valid URL!\n{}", e);
+   		std::process::exit(1);
+   }
+
    println!("{:?}", args);
+}
+
+fn validate_url(url: &str) -> Result<(), ParseError> {
+	let supported_urls = ["api.spotify.com"];
+	let parsed = Url::parse(url)?;
+
+	let base_url = match parsed.host_str() {
+		Some(s) => s,
+		None => ""
+	};
+
+	if supported_urls.contains(&base_url) {
+		return Ok(());
+	}
+
+	Err(ParseError::IdnaError)
 }
