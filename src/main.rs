5e132adf908e1622b1ee82d21011458a8764d483
Inumidun <ashamao90@gmnail.com>
2020-06-03 02:13:30 +0100

    Add structopt package to read and parse CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use structopt::StructOpt;
+
+
+#[derive(Debug, StructOpt)]
+#[structopt(name="Cohen", about="Compose your music like the great Leonard Cohen!")]
+struct ConfigArgs {
+	#[structopt(short)]
+	source: String,
+
+	#[structopt(short)]
+	destination: String,
+
+	#[structopt(short, default_value="")]
+	title: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+   let args = ConfigArgs::from_args();
+   println!("{:?}", args);
 }
